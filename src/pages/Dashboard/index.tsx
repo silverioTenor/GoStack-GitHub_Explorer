f2ec899f30138fbf8318cf7b5327ac8a1cafdb39
silverioTenor <silverio_tenor@outook.com>
2021-01-14 18:46:16 -0300

    Estruturação e estilização do dashboard e estilização global.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiChevronRight } from 'react-icons/fi';
+
+import { Header, Repositories } from './styles';
+
+import logo from '../../assets/logo.svg';
+import avatar from '../../assets/user.jpg';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="GitHub Explorer" />
+
+        <h1>Explore repositórios no GitHub.</h1>
+
+        <form>
+          <input type="text" placeholder="Digite o nome do repositório" />
+
+          <button type="submit">Pesquisar</button>
+        </form>
+      </Header>
+
+      <Repositories>
+        <Link to="/">
+          <img src={avatar} alt="" />
+
+          <div className="content">
+            <strong>silverioTenor/GoBarber</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Header = styled.header`
+  h1 {
+    width: 440px;
+    margin-top: 100px;
+    font-size: 48px;
+    color: #3a3a3a;
+  }
+
+  form {
+    max-width: 714px;
+    margin-top: 40px;
+
+    display: flex;
+    align-items: center;
+
+    input {
+      padding: 24px 30px;
+      flex: 1;
+      color: #3a3a3a;
+      border: 0;
+      border-radius: 5px 0 0 5px;
+
+      &::placeholder {
+        color: #a8a8b3;
+      }
+    }
+
+    button {
+      width: 210px;
+      padding: 24px 30px;
+      font-weight: bold;
+      color: #fff;
+      background: #04d361;
+      border: 0;
+      border-radius: 0 5px 5px 0;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#04d361')};
+      }
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  max-width: 714px;
+  margin-top: 120px;
+
+  a {
+    padding: 16px;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    background: #fff;
+    border-radius: 5px;
+    transition: all 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translatex(10px);
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+
+      object-fit: cover;
+    }
+
+    .content {
+      margin-left: 24px;
+
+      strong {
+        display: block;
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        display: block;
+        font-size: 16px;
+        color: #a8a8b3;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
